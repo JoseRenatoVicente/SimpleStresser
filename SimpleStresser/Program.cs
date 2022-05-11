f679ecd3c4a87eb7f15dc3b538b34b5616ec6e28
Jose Renato <josetq12@gmail.com>
2022-05-11 15:18:45 -0300

    Add project files.

---
+++ SimpleStresser/Program.cs
@@ -0,0 +1,25 @@
+﻿
+var tasks = new List<Task<HttpResponseMessage>>();
+int numberOfRequests = 2000;
+
+Parallel.For(0, numberOfRequests, count =>
+{
+    tasks.Add(MakeRequestAsync(new("https://example.com")));
+});
+
+Console.WriteLine("End");
+
+
+async Task<HttpResponseMessage> MakeRequestAsync(Uri uri)
+{
+    try
+    {
+        using var httpClient = new HttpClient();
+
+        return await httpClient.GetAsync(uri);
+    }
+    catch
+    {
+        return new HttpResponseMessage();
+    }
+}
